6ded810d6b935d9c0c703d5853a3a7293edd42c0
Vinodh Balaji Sridharan <Vinodh.balaji.sridharan@oracle.com>
2023-05-09 11:25:23 +0530

    Initial Commit

---
+++ rewardify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rewardify
+//
+//  Created by Vinodh Balaji Sridharan on 09/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rewardify/rewardifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rewardifyApp.swift
+//  rewardify
+//
+//  Created by Vinodh Balaji Sridharan on 09/05/23.
+//
+
+import SwiftUI
+
+@main
+struct rewardifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
